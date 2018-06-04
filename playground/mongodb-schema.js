61983e495d2cec9f11ef5259a96e1f0b685845de
gm019w <gm019w@att.com>
2018-06-05 02:36:26 +0530

    Created POST Api for Todo

---
+++ playground/mongodb-schema.js
@@ -0,0 +1,26 @@
+// var user = new User({
+//     email: ' gauravmurkute20@gmail.com'
+// });
+
+// user.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// },(e) => {
+//     console.log('Unable to save user',e);
+// });
+// var newTodo = new Todo({
+
+//     text: 'Cook dinner'
+// });
+
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log(e);
+// });
