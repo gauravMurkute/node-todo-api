a88f9ffe48d89c4d4e7c5d29d4cc8b91235e5063
gm019w <gm019w@att.com>
2018-06-10 23:25:24 +0530

    Added Update todo api

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
  var {mongoose} =  require('./db/mongoose.js');
  var {Todo} = require('./models/todo');
  var {User} = require('./models/user');
- var {ObjectID} = require('mongodb');
+ 
 
 
  var app = express();
@@ -88,6 +90,44 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+
+
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+    }
+    else
+    {
+        body.completed = false;
+        body.completedAt = null;
+
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+
+            if(!todo)
+            {
+                    return res.status(404).send();
+            }
+
+            res.send({todo});
+
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 
